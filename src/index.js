dee6de719e0530de47af01495ddb801e82df18f2
Ares Garcia <agarciap@ids.com.mx>
2020-03-15 22:00:13 -0600

    subida de archivos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-import { init } from './js/usuarios-page'
+// import * as CRUD from './js/crud';
+import { init } from './js/archivos-pages';
 
+
+// CRUD.getUsuario(1).then(console.log);
+
+// CRUD.crearUsuario({
+//     name: 'Fernando',
+//     job: 'Carpintero'
+// }).then(console.log);
+
+
+// CRUD.actualizarUsuario(1, {
+//     name: 'Melisa',
+//     job: 'developer'
+// }).then(console.log);
+
+// CRUD.borrarUsuario(1).then(console.log);
 
 init();
